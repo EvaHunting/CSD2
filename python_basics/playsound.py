33d639ccf5adabaf8a0cf6f6379af601b8014b66
Eva Hunting <evahunting@gmail.com>
2018-09-11 12:28:22 +0200

    version 1.0.5

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,7 +1,5 @@
 import simpleaudio as sa
 
-wave_obj = sa.WaveObject.from_wave_file("path/to/file.wav")
+wave_obj = sa.WaveObject.from_wave_file("/Users/evahunting/CSD2/python_basics/Quack.wav")
 play_obj = wave_obj.play()
 play_obj.wait_done()
-
-
