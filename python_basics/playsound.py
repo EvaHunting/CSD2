127cbde0d6a2f7994a04272eed375c415a5959f4
Eva Hunting <evahunting@gmail.com>
2018-09-17 15:03:45 +0200

    version 1.0.6

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,5 +1,17 @@
 import simpleaudio as sa
 
-wave_obj = sa.WaveObject.from_wave_file("/Users/evahunting/CSD2/python_basics/Quack.wav")
-play_obj = wave_obj.play()
-play_obj.wait_done()
+times = int(input("insert the number of times you would like to hear this sound: "));
+
+def total(times):
+
+    if total == 0:
+        print("Done");
+        return 0
+    else:
+        wave_obj = sa.WaveObject.from_wave_file("/Users/evahunting/CSD2/python_basics/Quack.wav")
+        play_obj = wave_obj.play()
+        play_obj.wait_done()    
+        print(times);
+        return(times - 1);
+
+total(times);
