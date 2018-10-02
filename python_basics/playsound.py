c91c8df8133c2bc8fbc7a30c9ae385a0eb10e03a
Eva Hunting <evahunting@gmail.com>
2018-10-02 11:02:44 +0200

    version 1.1.0

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -8,7 +8,7 @@ def total(times):
         print("Done");
         return 0
     else:
-        wave_obj = sa.WaveObject.from_wave_file("/Users/evahunting/CSD2/python_basics/Quack.wav")
+        wave_obj = sa.WaveObject.from_wave_file("/Users/evahunting/CSD2/python_basics/Snare.wav")
         play_obj = wave_obj.play()
         play_obj.wait_done()    
         print(times);
