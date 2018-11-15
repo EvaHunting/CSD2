00f1f880d095548fb7095291ef11efe8c0859d67
Eva Hunting <evahunting@gmail.com>
2018-11-15 17:56:37 +0100

    version 2.0.1

--- Cpp/instrument.cpp
+++ Cpp/instrument.cpp
@@ -4,10 +4,16 @@ class Instrument  {
 public:             // public acces specifier
   // declare constructors
   Instrument();
-  Instrument(std::string sound);
+  Instrument(std::string snd);
+
+  std::string sound;
 
   // methods (class function)
   void makesound() {
-  std::cout << "sound" << std::endl;
+  std::cout << sound << std::endl;
+  }
+
+  void play()  {
+    makesound();
   }
 };
