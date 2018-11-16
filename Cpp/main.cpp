66730a05abc8b805df94d252f850a19de27b46e8
Eva Hunting <evahunting@gmail.com>
2018-11-16 16:30:17 +0100

    version 2.0.3

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -8,15 +8,16 @@ public:             // public acces specifier
   Instrument(std::string snd, int ptch);
   Instrument(std::string snd, int ptch, int volm);
 
+  //  variables
   std::string sound;
+  int pitch;
+  int volume;
 
   // methods (class function)
   void makesound() {
   std::cout << sound << std::endl;
   }
 
-  int pitch;
-  int volume;
 
   void play()  {
     makesound();
@@ -41,10 +42,23 @@ Instrument::Instrument(std::string snd, int ptch, int volm) {
   std::cout << "volume: " << volm << std::endl;
 }
 
-int main(int argc, char* argv[])  {
-  Instrument instrument1("Tuut", 440, 100);
+int main()  {
+  int hulp;
+  Instrument instrument1("Tuut");
   instrument1.play();
-  Instrument instrument2("Biem", 500, 127);
+  std::cout << "Pitch: ";
+  std::cin >> hulp;
+  instrument1.pitch = hulp;
+  std::cout << "Volume: ";
+  std::cin >> hulp;
+  instrument1.volume = hulp;
+  Instrument instrument2("Biem");
   instrument2.play();
+  std::cout << "Pitch: ";
+  std::cin >> hulp;
+  instrument2.pitch = hulp;
+  std::cout << "Volume: ";
+  std::cin >> hulp;
+  instrument2.volume = hulp;
 return(0);
 }
